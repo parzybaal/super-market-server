5da65d4be45fec9edeb3f51e55db06606068d064
Federico Paglia <federicoepaglia@gmail.com>
2023-09-26 11:59:24 -0300

    SvConfig-Prisma-CorsInstallation

---
+++ src/config/cors/cors.ts
@@ -0,0 +1,6 @@
+export const CORS = {
+    origin: true,
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
+    //Seguridad de las rutas, por el momento en FALSE:
+    credentials: false //CAMBIAR LUEGO
+}
