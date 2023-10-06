23960f9d97063a3561a33b6bcbc82888e7f718bd
Juan <juampi.parzybal@gmail.com>
2023-10-06 11:19:20 -0300

    second commit

--- src/config/cors/cors.ts
+++
@@ -1,6 +0,0 @@
-export const CORS = {
-    origin: true,
-    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
-    //Seguridad de las rutas, por el momento en FALSE:
-    credentials: false //CAMBIAR LUEGO
-}
